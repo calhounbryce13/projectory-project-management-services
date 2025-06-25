2faead469d7e8d412c2f9b0f888323613578f8c9
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-06-25 11:52:04 -0700

    file transfer

---
+++ model.py
@@ -0,0 +1,95 @@
+"""
+Description: Microservice model file for Projectory main program, using MongoEngine 
+to communicate with the database
+Author: Bryce Calhoun
+"""
+
+from mongoengine import *
+import config
+
+
+
+connect(host=config.data['MONGODB_CONNECTION_STRING'])
+
+
+
+class Task(EmbeddedDocument):
+    meta = {"strict": False}
+
+    task_description = StringField()
+    is_complete = IntField()
+
+
+
+
+class Complete(EmbeddedDocument):
+    title = StringField()
+    goal = StringField()
+
+
+class Planned(EmbeddedDocument):
+    meta = {"strict": False}
+
+    title = StringField()
+
+
+
+class Current(EmbeddedDocument):
+    meta = {"strict": False}
+
+    title = StringField()
+    goal = StringField()
+    tasks = ListField(EmbeddedDocumentField(Task))
+    is_complete = IntField()
+
+
+class User(Document):
+
+    meta = {"strict": False, "collection": "user-data"}
+
+    email = StringField()
+    current = ListField(EmbeddedDocumentField(Current))
+    planned = ListField(EmbeddedDocumentField(Planned))
+    complete = ListField(EmbeddedDocumentField(Complete))
+
+
+
+def mark_project_complete(userEmail, projectTitle):
+    user = User.objects(email=userEmail).first()
+
+    if(user):
+        goal = ""
+        for currentProject in user.current:
+            if(currentProject.title == projectTitle):
+                goal = currentProject.goal
+                break
+        print("abacus")
+        user.complete.append(Complete(title=projectTitle, goal=goal))
+        user.save()
+        return True
+    return False
+
+    
+
+
+def mark_project_task(UserEmail, projectTitle, taskIndex, mark):
+    if((mark != 0) and (mark != 1)): return False
+
+    user = User.objects(email=UserEmail).first()
+    if(user):
+        current_projects_updated = user.current
+
+        for currentProject in current_projects_updated:
+            if(currentProject.title) == projectTitle:
+                try:
+                    currentProject.tasks[taskIndex].is_complete = mark
+                    break
+                except IndexError:
+                    print("\nError: Given task index is out of range!")
+                    return False
+        
+        user.current = current_projects_updated
+        user.save()
+        return True
+    return False
+    
---
+++ server.py
@@ -0,0 +1,83 @@
+"""
+Description: Microservice controller file for Projectory main program, uses another microservice for deletion
+Author: Bryce Calhoun
+"""
+
+
+from flask import Flask, request
+from flask_cors import CORS
+import requests
+import model
+import json
+
+
+
+
+PORT = 5000
+app = Flask(__name__)
+CORS(app, supports_credentials=True, origins=["http://127.0.0.1:5500"])
+
+
+def validate_request(email, title, index, mark):
+    if(email is not None and title is not None and index is not None and mark is not None):
+        if(isinstance(email, str)):
+            if(isinstance(title, str)):
+                if(isinstance(index, int)):
+                    if(isinstance(mark, int)):
+                        return True
+    return False
+
+
+def delete_project_after_completion(userEmail, projectName):
+    headers = {
+            "Content-Type": "application/json",
+            "x-user-email": userEmail
+            }
+    data = {
+            "project-type": "current",
+            "project-name": projectName
+    }
+
+    try:
+        deleteProjectResponse = requests.delete('http://127.0.0.1:8000/deletion', headers=headers, json=data)
+        return True
+    except Exception:
+        return False
+
+
+@app.route('/task-manager', methods=['POST'])
+def call_model_to_mark_task():
+    try:
+        userEmail, projectTitle, taskIndex, mark = (request.json).values()
+    except ValueError:
+        return "Error, Invalid request", 400
+    
+    if(validate_request(userEmail, projectTitle, taskIndex, mark)):
+        status = model.mark_project_task(UserEmail=userEmail, projectTitle=projectTitle, taskIndex=taskIndex, mark=mark)
+        if(status == False):
+            return "Error, Issue communicating with database" ,500
+        return "success", 200
+        
+    return "Error, Invalid request", 400
+
+
+
+@app.route('/completed-project-manager', methods=['PUT'])
+def call_model_to_complete_project():
+    print("endpoint reached")
+
+
+    userEmail, projectTitle = (request.json).values()
+
+    result = model.mark_project_complete(userEmail=userEmail, projectTitle=projectTitle)
+    if(result):
+        deleted = delete_project_after_completion(userEmail=userEmail, projectName=projectTitle)
+        if(deleted):
+            return "success", 200
+    return "fail", 500
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=PORT)
