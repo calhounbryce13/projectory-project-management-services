c023b8ead8702ee294ae5d2b209b952a07f28621
calhounbryce13 <111155940+calhounbryce13@users.noreply.github.com>
2025-06-29 15:41:05 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,17 +3,21 @@ Description: project management service controller file for Projectory main prog
 Author: Bryce Calhoun
 """
 
-from flask import Flask, request
+from flask import Flask, request, make_response
 from flask_cors import CORS
 import requests
 import model
 import os
 
+#todo: I need to add functionality to the endpoints
+#todo: that will check the incoming method and set the proper 
+#todo: headers for a preflight request
+
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True, resources={r"/*":
                                                 {"origins": "https://calhounbryce13.github.io",
-                                                "methods": ["GET", "POST", "PUT", "OPTIONS"],
+                                                "methods": ["POST", "PUT", "OPTIONS"],
                                                 "allow_headers": ["Content-Type"]
                                                 }})
 
@@ -45,6 +49,14 @@ def delete_project_after_completion(userEmail, projectName):
 
 @app.route('/task-manager', methods=['POST', 'OPTIONS'])
 def call_model_to_mark_task():
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers["Access-Control-Allow-Origin"] = "https://calhounbryce13.github.io"
+        response.headers["Access-Control-Allow-Methods"] = "POST, OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        response.status_code = 200
+        return response
     try:
         userEmail, projectTitle, taskIndex, mark = (request.json).values()
     except ValueError:
